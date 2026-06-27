ea6c2ef84299c4f944966f9f6d804d00e70793fe
phamdjjd6-create <phamdjjd6@gmail.com>
2026-06-27 09:37:10 +0700

    [DE191029] chore: Xoa 22 file rac o thu muc goc (scripts test, payload, file tam)

--- check_login.js
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:8081/api/auth/login", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ username: "Police157", password: "password" })
-}).then(res => res.json()).then(console.log).catch(console.error);
